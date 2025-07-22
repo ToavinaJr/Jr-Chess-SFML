3fad67199d37abb8723c1d9a28c018a5271b440b
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-07-22 21:15:23 +0300

    Modification de l'indentation

--- include/Board.hpp
+++ include/Board.hpp
@@ -24,26 +24,32 @@ namespace Jr {
             int                                                 enPassantSquare = -1;
 
             // Variables pour savoirs la possibilité de roquer
-            bool whiteKingMoved = false;
-            bool whiteRookKingsideMoved = false;
-            bool whiteRookQueensideMoved = false;
+            bool                                                whiteKingMoved             =    false;
+            bool                                                whiteRookKingsideMoved     =    false;
+            bool                                                whiteRookQueensideMoved    =    false;
 
-            bool blackKingMoved = false;
-            bool blackRookKingsideMoved = false;
-            bool blackRookQueensideMoved = false;
+            bool                                                blackKingMoved             =    false;
+            bool                                                blackRookKingsideMoved     =    false;
+            bool                                                blackRookQueensideMoved    =    false;
 
-            void loadTextures();
-            void updateSpritesFromBitboards();
-            bool movePiece(int from, int to);
-            std::vector<int> getLegalMoves(const std::string& pieceName, int from, bool forCheck = false);
+            // Variables pour la promotion des pièces
+            bool                                                promotionPending           = false;
+            bool                                                promotionWhite             = true;
+            int                                                 promotionSquare            = -1;
+            std::vector<sf::Sprite>                             promotionChoices;
 
-            bool isKingInCheck(bool whiteKing);
-            bool wouldBeInCheck(int from, int to, bool whiteKing);
+            void                                                loadTextures();
+            void                                                updateSpritesFromBitboards();
+            bool                                                movePiece(int from, int to);
+            std::vector<int>                                    getLegalMoves(const std::string& pieceName, int from, bool forCheck = false);
+
+            bool                                                isKingInCheck(bool whiteKing);
+            bool                                                wouldBeInCheck(int from, int to, bool whiteKing);
 
         public:
             Board();
-            void draw(sf::RenderWindow& window);
-            void handleClick(int mouseX, int mouseY);
+            void                                                draw(sf::RenderWindow& window);
+            void                                                handleClick(int mouseX, int mouseY);
     };
 }
 
